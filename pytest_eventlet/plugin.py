dbf87331bcd6b2c5ec520866ee01f55d31f106ef
Gabriel Juchault <gabriel.juchault@gmail.com>
2026-01-07 14:34:31 +0100

    fix: do not mock thread

--- pytest_eventlet/plugin.py
+++ pytest_eventlet/plugin.py
@@ -3,4 +3,4 @@ def pytest_load_initial_conftests():
     # make sure we monkey_patch before local conftests
     import eventlet
 
-    eventlet.monkey_patch()
+    eventlet.monkey_patch(thread=False)
